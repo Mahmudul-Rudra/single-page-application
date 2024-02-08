c27610f6a57d32ea38240b5ea2c2658c4fd0781e
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-02-08 20:17:16 +0600

    Second home section added with styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-
+import vg from "../assets/2.webp";
 const Home = () => {
   return (
-    <div className="home" id="home">
-      <main>
-        <h1>Single Page</h1>
-        <p>A single page application which is my first React Application</p>
-      </main>
-    </div>
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Single Page</h1>
+          <p>A single page application which is my first React Application</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            This is my first React application. It is a simple single page web
+            application. Further I will make more complex application day by
+            day.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
