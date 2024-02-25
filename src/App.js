62d27a40a7ea04db82e09aa514e1b61659494dd7
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-02-25 22:23:20 +0600

    Contact component added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
+import "./styles/contact.scss";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
