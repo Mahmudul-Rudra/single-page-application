92964a5137170b567882662fe390566df842cea9
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-02-25 20:24:26 +0600

    Third home element style is adding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,15 +9,35 @@ const Home = () => {
           <p>A single page application which is my first React Application</p>
         </main>
       </div>
-
+      /////////////////////////////////////////////////////////////////////
       <div className="home2">
         <img src={vg} alt="Graphics" />
-
         <div>
           <p>
             This is my first React application. It is a simple single page web
             application. Further I will make more complex application day by
             day.
+          </p>
+        </div>
+      </div>
+      /////////////////////////////////////////////////////////////////////
+      <div className="home3">
+        <div>
+          <h1>Who am I?</h1>
+          <p>
+            This is S. M. Mahmudul Hasan Rudra recently completed undergrad from
+            BRAC University and eager to embark on the dynamic Job Industry. I
+            think my analytical skills will have a great impact in you sector.
+            <br />
+            During my undergraduation I have done a lot of projects which is
+            based on React JS. Along with this projects I have also worked using
+            Java, Python, C++, github. I hope these technical skills will help
+            me to give the best technical support to your industry.
+            <br />
+            In BRAC University, I served BRAC University Entrepreneurship
+            Development Forum as Assistant Director. During this time, I have
+            conducted two big events which was with the CEO of Nagad and an
+            Intra Case Competition where my team was the runner up.
           </p>
         </div>
       </div>
